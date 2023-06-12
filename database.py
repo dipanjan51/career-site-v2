a126226bf3ac5835d6fbf351a4d9629ecd471f2b
dipanjan51 <dipanjandeb1995@gmail.com>
2023-06-12 20:47:32 +0530

    dynamic pages for each job and removed replit files

--- database.py
+++ database.py
@@ -18,11 +18,21 @@ engine = create_engine(
 )
 
 def load_jobs_from_db():
-  with engine.connect() as conn:
-    result = conn.execute(text("select * from jobs"))
-    jobs = []
-    for row in result.all():
-        jobs.append(row._asdict())
-    return jobs
+    with engine.connect() as conn:
+        result = conn.execute(text("select * from jobs"))
+        jobs = []
+        for row in result.all():
+            jobs.append(row._asdict())
+        return jobs
 
-
+def load_job_from_db(id):
+    with engine.connect() as conn:
+        result = conn.execute(text(f"SELECT * FROM jobs WHERE id = {id}"))
+        # query = text("SELECT * FROM jobs WHERE id = :val")
+        # result = conn.execute(query.params(val=id))
+        rows = result.all()
+        if len(rows) == 0:
+            return None
+        else:
+            return rows[0]._asdict()
+      
